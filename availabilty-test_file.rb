b4f97cb9efee4ad5377e303d412fdcbde1798b39
Shamseera <kurumbeths@gmail.com>
2019-11-16 14:37:25 +1100

    modified availability-test-file

---
+++ availabilty-test_file.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative 'appointment-class'
+# require_relative 'main'
+require_relative 'methods'
+
+def test1_check_availability
+  fake_list = []
+  actual_doctor_name = 'peter'
+  actual_date = '12/4/2019'
+  actual_time = '2'
+  full_name = 'shamseera'
+  dob = '18/10/1991'
+  mobile_num = '470345722'
+  expected_doctor_name = 'lucy'
+  expected_date = '13/11/2019'
+  expected_time = '3'
+
+  appointment = Appointment.new(actual_doctor_name, actual_date, actual_time, full_name, dob, mobile_num)
+  fake_list << appointment
+  if check_availability(expected_doctor_name, expected_date, expected_time, fake_list)
+    puts 'fail'
+  else
+    puts 'pass'
+   end
+end
+
+def test2_check_availability
+  fake_list = []
+  actual_doctor_name = 'peter'
+  actual_date = '12/4/2019'
+  actual_time = '2'
+  full_name = 'shamseera'
+  dob = '18/10/1991'
+  mobile_num = '470345722'
+  expected_doctor_name = 'peter'
+  expected_date = '12/4/2019'
+  expected_time = '2'
+
+  appointment = Appointment.new(actual_doctor_name, actual_date, actual_time, full_name, dob, mobile_num)
+  fake_list << appointment
+  if check_availability(expected_doctor_name, expected_date, expected_time, fake_list)
+    puts 'fail'
+  else
+    puts 'pass'
+      end
+  end
+
+test1_check_availability
+test2_check_availability
