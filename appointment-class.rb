bb001bb355704165f5fcb3cbf230ab39601e2e61
Shamseera <kurumbeths@gmail.com>
2019-11-16 12:18:54 +1100

    removed medicare num

--- appointment-class.rb
+++ appointment-class.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Appointment
-  attr_accessor :doctor_name, :date, :time, :full_name, :mobile_num, :dob, :medicare_num
-  def initialize(doctor_name, date, time, full_name, dob, mobile_num, medicare_num)
+  attr_accessor :doctor_name, :date, :time, :full_name, :mobile_num, :dob
+  def initialize(doctor_name, date, time, full_name, dob, mobile_num)
     @doctor_name = doctor_name
     @date = date
     @time = time
     @full_name = full_name
     @dob = dob
     @mobile_num = mobile_num
-    @medicare_num = medicare_num
   end
   end
