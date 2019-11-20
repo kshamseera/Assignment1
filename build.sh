ac2abbf6cf480798df1f55a0ce1b3380096ca632
Shamseera <kurumbeths@gmail.com>
2019-11-20 21:37:34 +1100

    bash file added

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# make a distribution directory
+mkdir -p dist
+
+# copy Ruby files to the distribution directory
+cp ./main.rb ./dist
+cp ./appointment-class.rb ./dist
+cp ./methods.rb ./dist
+
+
+# install required gems
+gem install colorize
+gem install tty-prompt
